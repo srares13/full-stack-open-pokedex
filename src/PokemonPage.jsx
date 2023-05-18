9a46f969bdbaf18c15bdb6a474c8b80f638474f6
Rares Suciu <rares.s013@gmail.com>
2023-05-18 13:59:07 +0300

    m6

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -19,13 +19,16 @@ const PokemonPage = ({ previous, next }) => {
   }
 
   const { type } = pokemon.types.find((type) => type.slot === 1)
-  const stats = pokemon.stats.map((stat) => ({
-    name: formatName(stat.stat.name),
-    value: stat.base_stat
-  })).reverse()
+  const stats = pokemon.stats
+    .map((stat) => ({
+      name: formatName(stat.stat.name),
+      value: stat.base_stat
+    }))
+    .reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
+  // eslint-disable-next-line no-console
   console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
@@ -35,7 +38,10 @@ const PokemonPage = ({ previous, next }) => {
         {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
-        <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
+        <div
+          className="pokemon-image"
+          style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }}
+        />
         <div className="pokemon-info">
           <div className="pokemon-name">{pokemon.name}</div>
           <div className="pokemon-stats" data-testid="stats">
@@ -51,8 +57,12 @@ const PokemonPage = ({ previous, next }) => {
             </table>
           </div>
           <div className="pokemon-abilities">
-            {normalAbility && <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />}
-            {hiddenAbility && <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />}
+            {normalAbility && (
+              <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />
+            )}
+            {hiddenAbility && (
+              <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />
+            )}
           </div>
         </div>
       </div>
